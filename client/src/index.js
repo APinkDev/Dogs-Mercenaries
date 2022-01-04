0683faeb051356e6ff2ea34582335de56e3d524d
Neko <justapinkdev@gmail.com>
2022-01-04 20:02:13 -0300

    version for heroku

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios"
 import "./index.css";
 
 import App from "./App";
@@ -7,6 +8,10 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter as Router } from "react-router-dom";
 import store from "./Store/store";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
